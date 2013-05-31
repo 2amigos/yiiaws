5cd040fed8d4c05313b1260b8c014ed21e36ba82
Antonio Ramirez <antonio@ramirezcobos.com>
2013-05-31 12:51:56 +0200

    dynamic configurations

--- components/A2Base.php
+++ components/A2Base.php
@@ -15,78 +15,87 @@ use Guzzle\Inflection\Inflector;
 
 abstract class A2Base extends CComponent
 {
-	const AWS_AUTOSCALING = 'autoscaling';
-	const AWS_CLOUDFRONT = 'cloudfront';
-	const AWS_CLOUDWATCH = 'cloudwatch';
-	const AWS_DATAPIPELINE = 'datapipeline';
-	const AWS_DYNAMODB = 'dynamodb';
-	const AWS_EC2 = 'ec2';
-	const AWS_ELASTIC_BEANSTALK = 'elasticbeanstalk';
-	const AWS_ELASTIC_LOADBALANCING = 'elasticloadbalancing';
-	const AWS_ELASTIC_TRANSCODER = 'elastictranscoder';
-	const AWS_GLACIER = 'glacier';
-	const AWS_IAM = 'iam';
-	const AWS_OPSWORKS = 'opsworks';
-	const AWS_RDS = 'rds';
-	const AWS_REDSHIFT = 'redshift';
-	const AWS_ROUTE53 = 'route53';
-	const AWS_S3 = 's3';
-	const AWS_SES = 'ses';
-	const AWS_SIMPLEDB = 'simpledb';
-	const AWS_SNS = 'sns';
-	const AWS_SQS = 'sqs';
-	const AWS_STS = 'sts';
-	const AWS_SWF = 'swf';
+    const AWS_AUTOSCALING           = 'autoscaling';
+    const AWS_CLOUDFRONT            = 'cloudfront';
+    const AWS_CLOUDWATCH            = 'cloudwatch';
+    const AWS_DATAPIPELINE          = 'datapipeline';
+    const AWS_DYNAMODB              = 'dynamodb';
+    const AWS_EC2                   = 'ec2';
+    const AWS_ELASTIC_BEANSTALK     = 'elasticbeanstalk';
+    const AWS_ELASTIC_LOADBALANCING = 'elasticloadbalancing';
+    const AWS_ELASTIC_TRANSCODER    = 'elastictranscoder';
+    const AWS_GLACIER               = 'glacier';
+    const AWS_IAM                   = 'iam';
+    const AWS_OPSWORKS              = 'opsworks';
+    const AWS_RDS                   = 'rds';
+    const AWS_REDSHIFT              = 'redshift';
+    const AWS_ROUTE53               = 'route53';
+    const AWS_S3                    = 's3';
+    const AWS_SES                   = 'ses';
+    const AWS_SIMPLEDB              = 'simpledb';
+    const AWS_SNS                   = 'sns';
+    const AWS_SQS                   = 'sqs';
+    const AWS_STS                   = 'sts';
+    const AWS_SWF                   = 'swf';
 
 
-	protected $_aws;
+    protected $_aws;
 
-	protected $_client;
+    protected $_client;
 
-	/**
-	 * Returns the aws service builder
-	 * @return Guzzle\Service\Builder\ServiceBuilder
-	 * @throws CException
-	 */
-	public function getAws()
-	{
-		if (null === $this->_aws)
-		{
-			$config = __DIR__ . '/../config/aws-config.php';
-			if (!@file_exists($config))
-				throw new CException(Yii::t('zii', '"aws-config.php" configuration file not found'));
+    protected $_config;
 
-			$this->_aws = Aws::factory($config);
-		}
+    /**
+     * Added option to use dynamic configurations instead of a file
+     * @param array $config
+     */
+    public function __construct($config = array())
+    {
+        if (!empty($config))
+            $this->_config = $config;
+    }
 
-		return $this->_aws;
-	}
+    /**
+     * Returns the aws service builder
+     * @return Guzzle\Service\Builder\ServiceBuilder
+     * @throws CException
+     */
+    public function getAws()
+    {
+        if (null === $this->_aws) {
+            $config = $this->_config !== null ? $this->_config : __DIR__ . '/../config/aws-config.php';
+            if (is_scalar($config) && !@file_exists($config))
+                throw new CException(Yii::t('zii', '"aws-config.php" configuration file not found'));
 
-	/**
-	 * Magic call to wrapped amazon aws methods. If not command found, then call parent component
-	 * @param string $method
-	 * @param array $args
-	 * @return mixed
-	 * @throws CException
-	 */
-	public function __call($method, $args)
-	{
-		$args = current($args);
-		try
-		{
-			$command = $this->getClient()
-				->getCommand(Inflector::getDefault()->camel($method), $args);
+            $this->_aws = Aws::factory($config);
+        }
 
-			if ($command)
-				return $this->getClient()->execute($command, $args);
+        return $this->_aws;
+    }
 
-		} catch (Exception $e)
-		{
-			// do nothing
-			throw new CException(Yii::t('zii', $e->getMessage())); //custom added
-		}
-		return parent::__call($method, $args);
-	}
+    /**
+     * Magic call to wrapped amazon aws methods. If not command found, then call parent component
+     * @param string $method
+     * @param array $args
+     * @return mixed
+     * @throws CException
+     */
+    public function __call($method, $args)
+    {
+        $args = current($args);
+        try {
+            $command = $this->getClient()
+                ->getCommand(Inflector::getDefault()->camel($method), $args);
 
-	abstract public function getClient();
+            if ($command)
+                return $this->getClient()->execute($command, $args);
+
+        } catch (Exception $e) {
+            // do nothing
+            throw new CException(Yii::t('zii', $e->getMessage())); //custom added
+        }
+        return parent::__call($method, $args);
+    }
+
+    abstract public function getClient();
 }
